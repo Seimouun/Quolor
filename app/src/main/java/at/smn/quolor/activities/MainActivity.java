c7efd2bb9b8c94879ddaa205f83e56481850893b
Seimouun <simon.reitmann>
2021-05-26 15:58:04 +0200

    Initial Design
    Initial app design done

--- app/src/main/java/at/smn/quolor/activities/MainActivity.java
+++ app/src/main/java/at/smn/quolor/activities/MainActivity.java
@@ -1,8 +1,9 @@
-package at.smn.quolor;
+package at.smn.quolor.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import at.smn.quolor.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/at/smn/quolor/activities/fragments/LightsFragment.java
@@ -0,0 +1,14 @@
+package at.smn.quolor.activities.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LightsFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lights_fragment);
+    }
+}
